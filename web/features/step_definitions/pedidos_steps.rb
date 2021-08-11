6e6838849caf1147f761676853e95c8da3d4856d
Andreza Silva <dedezasilva@live.com>
2021-08-11 09:23:39 -0400

    aula 11, ultima aula desse modulo, então teste de regressão

--- web/features/step_definitions/pedidos_steps.rb
+++ web/features/step_definitions/pedidos_steps.rb
@@ -3,7 +3,7 @@ Dado('que meu perfil de anúnciante é {string} e {string}') do |email, password
     @pass_anunciante = password
   end
   
-  Dado('que eu tenho o seguinte equipamento cadastrado:') do |table|
+Dado('que eu tenho o seguinte equipamento cadastrado:') do |table|
     user_id = SessionsService.new.get_user_id(@email_anunciante, @pass_anunciante)
 
 
@@ -20,7 +20,7 @@ MongoDB.new.remove_equipo(@equipo[:name], @email_anunciante)
 
   result = EquiposService.new.create(@equipo, user_id)
   @equipo_id = result.parsed_response["_id"]
-  log @equipo_id
+  # log @equipo_id
 end
   
 Dado('acesso o meu dashboard') do
@@ -30,25 +30,24 @@ Dado('acesso o meu dashboard') do
     expect(@dash_page.on_dash?).to be true
 end
   
-Quando('{string} e {string} solicita a locação desse equipo') do |string, string2|
-    def booking(equipo_id, user_locator_id) 
-        return self.class.post(
-          "/equipos/#{equipo_id}/bookings",
-          body: { date: Time.now.strftime("%d/%m/#Y") }.to_json,
-          headers: {
-                  "user_id": user_locator_id, 
-                 },
-        )
-    
-    
-      end
+Quando('{string} e {string} solicita a locação desse equipo') do |email, password|
+    user_id = SessionsService.new.get_user_id(email, password)
+    EquiposService.new.booking(@equipo_id, user_id)
 end
   
 Então('devo ver a seguinte mensagem:') do |doc_string|
-    pending # Write code here that turns the phrase above into concrete actions
+    expect_message = doc_string.gsub("DATA_ATUAL", Time.now.strftime("%d/%m/#Y"))
+
+    # order = find(".notifications p")
+    expect(@dash_page.order).to have_text expect_message
 end
   
-Então('devo ver os links: {string} e {string} no pedido') do |string, string2|
-    pending # Write code here that turns the phrase above into concrete actions
+Então('devo ver os links: {string} e {string} no pedido') do |button_accept, button_reject|
+  #  expect(page).to have_selector ".notifications button", text: button_accept
+  #  expect(page).to have_selector ".notifications button", text: button_reject
+
+
+   expect(@dash_page.order_actions(button_accept)).to be true
+   expect(@dash_page.order_actions(button_reject)).to be true
 end
 
